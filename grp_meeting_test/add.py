eaa5c93df4cc6541db9fc3f8999d8469428b8251
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-11-28 15:35:34 +0000

    feat!: adding new param "c" to main command

--- grp_meeting_test/add.py
+++ grp_meeting_test/add.py
@@ -1,16 +1,17 @@
 import argparse
 
 
-def add(a: int, b: int) -> int:
-    return a + b
+def add(a: int, b: int, c: int) -> int:
+    return a + b + c
 
 
 def main() -> None:
     parser = argparse.ArgumentParser("Add two numbers")
     parser.add_argument("a", type=int)
     parser.add_argument("b", type=int)
+    parser.add_argument("c", type=int)
     args = parser.parse_args()
-    print(add(args.a, args.b))
+    print(add(args.a, args.b, args.c))
 
 
 if __name__ == "__main__":
